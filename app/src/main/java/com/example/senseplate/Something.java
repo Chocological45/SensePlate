91d3ddf1cf966dfb6ea109af3abf7dc48b186605
Chocological <saptarshinath12@gmail.com>
2019-10-26 02:42:35 +0100

    Created Something.java

---
+++ app/src/main/java/com/example/senseplate/Something.java
@@ -0,0 +1,16 @@
+package com.example.senseplate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Something extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        //Hello There
+    }
+}
