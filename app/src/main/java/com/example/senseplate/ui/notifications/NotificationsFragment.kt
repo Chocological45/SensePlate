84343674bec707404dbfe40c503d49b9dae14dc2
sn4577 <saptarshinath12@gmail.com>
2020-03-01 16:01:33 +0000

    Moved bar chart to fragment from MainActivity.kt

--- app/src/main/java/com/example/senseplate/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/senseplate/ui/notifications/NotificationsFragment.kt
@@ -1,24 +1,16 @@
 package com.example.senseplate.ui.notifications
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.example.senseplate.DActivity
 import com.example.senseplate.R
 import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_dashboard.*
-import kotlinx.android.synthetic.main.fragment_notifications.*
 
 class NotificationsFragment : Fragment() {
 
@@ -34,19 +26,35 @@ class NotificationsFragment : Fragment() {
             ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_notifications, container, false)
 
-        //val textView: TextView = root.findViewById(R.id.text_notifications)
-        //notificationsViewModel.text.observe(this, Observer {
-        //textView.text = it
-        // })
-        // val btn1: Button = root.findViewById(R.id.date)
-//
-        // btn1.setOnClickListener {
-        //     val intent = Intent(getActivity(), DActivity::class.java)
-        //     startActivity(intent)
-        // }
+        val entries = ArrayList<BarEntry>()
+        entries.add(BarEntry(8f, 0))
+        entries.add(BarEntry(2f, 1))
+        entries.add(BarEntry(5f, 2))
+        entries.add(BarEntry(20f, 3))
+        entries.add(BarEntry(15f, 4))
+        entries.add(BarEntry(19f, 5))
 
+        val barDataSet = BarDataSet(entries, "Cells")
+
+        val labels = ArrayList<String>()
+        labels.add("18-Jan")
+        labels.add("19-Jan")
+        labels.add("20-Jan")
+        labels.add("21-Jan")
+        labels.add("22-Jan")
+        labels.add("23-Jan")
+        val data = BarData(labels, barDataSet)
+
+        val barChart = root.findViewById<BarChart>(R.id.barChart)
+        barChart.data = data // set the data and list of lables into chart
+
+        barChart.setDescription("Set Bar Chart Description")  // set the description
+
+        //barDataSet.setColors(ColorTemplate.COLORFUL_COLORS)
+        barDataSet.color = resources.getColor(R.color.colorAccent)
+
+        barChart.animateY(5000)
 
         return root
     }
-
 }
